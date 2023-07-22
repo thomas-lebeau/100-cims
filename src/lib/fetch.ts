d0e11ae11dd70da1b14649be6cf60f2ad33049d0
Thomas Lebeau <lebeau.thomas@gmail.com>
2023-07-22 17:40:17 +0200

    chore(*): Fixup

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -3,9 +3,12 @@ import { headers } from 'next/headers';
 type RequestInfo = Request | string;
 
 async function _fetch(input: RequestInfo | URL): Promise<Response> {
-  return await fetch(`${input}`, {
-    headers: headers(),
-  });
+  return await fetch(
+    `https://100-cims-git-next-thomas-lebeau.vercel.app${input}`,
+    {
+      headers: headers(),
+    }
+  );
 }
 
 export default _fetch;
