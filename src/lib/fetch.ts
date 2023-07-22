7e970e07d2b7a86ef5f1f9530293ff80570c2a6c
Thomas Lebeau <lebeau.thomas@gmail.com>
2023-07-22 17:18:16 +0200

    chore(*): Fixup

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,12 +1,12 @@
-import { headers } from 'next/headers';
+// import { headers } from 'next/headers';
 
 type RequestInfo = Request | string;
 
 async function _fetch(input: RequestInfo | URL): Promise<Response> {
-  const host = headers().get('host');
-  const protocol = host?.includes('localhost') ? 'http' : 'https';
+  // const host = headers().get('host');
+  // const protocol = host?.includes('localhost') ? 'http' : 'https';
 
-  return await fetch(`${protocol}://${host}${input}`, {
+  return await fetch(input /* `${protocol}://${host}${input}` */, {
     // headers: headers(),
   });
 }
