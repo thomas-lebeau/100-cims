44c41c4b741f33599f4e3ad801ad8976f597f73a
Thomas Lebeau <lebeau.thomas@gmail.com>
2023-07-22 17:02:50 +0200

    test

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,17 +1,12 @@
-import { RequestInit } from 'next/dist/server/web/spec-extension/request';
 import { headers } from 'next/headers';
 
 type RequestInfo = Request | string;
 
-async function _fetch(
-  input: RequestInfo | URL,
-  init: RequestInit = {}
-): Promise<Response> {
+async function _fetch(input: RequestInfo | URL): Promise<Response> {
   const host = headers().get('host');
   const protocol = host?.includes('localhost') ? 'http' : 'https';
 
   return await fetch(`${protocol}://${host}${input}`, {
-    ...init,
     headers: headers(),
   });
 }
