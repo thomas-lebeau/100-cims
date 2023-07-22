da1466ce7f84647a92c70406f92f37a68d2d3ddb
Thomas Lebeau <lebeau.thomas@gmail.com>
2023-07-22 10:23:10 +0200

    chore(*): Fixup

--- src/app/api/user/cims/route.ts
+++ src/app/api/user/cims/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: NextRequest) {
 
   if (!sessionToken) {
     return NextResponse.json(
-      { error: 'sessionToken not found' },
+      { error: `sessionToken "${cookieName}" not found` },
       { status: 301 }
     );
   }
