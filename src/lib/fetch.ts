033dfb440810b7f4f7c8f3d9571ef05ec33dad60
Thomas Lebeau <lebeau.thomas@gmail.com>
2023-07-22 17:27:19 +0200

    fixup

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,4 +1,4 @@
-// import { headers } from 'next/headers';
+import { headers } from 'next/headers';
 
 type RequestInfo = Request | string;
 
@@ -6,13 +6,13 @@ async function _fetch(input: RequestInfo | URL): Promise<Response> {
   // const host = headers().get('host');
   // const protocol = host?.includes('localhost') ? 'http' : 'https';
 
-  return await fetch(
-    'https://100-cims-git-next-thomas-lebeau.vercel.apps' +
-      input /* `${protocol}://${host}${input}` */,
-    {
-      // headers: headers(),
-    }
-  );
+  const headersData = headers();
+  const protocol = headersData.get('x-forwarded-proto');
+  const host = headersData.get('host');
+
+  return await fetch(`${protocol}://${host}${input}`, {
+    headers: headers(),
+  });
 }
 
 export default _fetch;
