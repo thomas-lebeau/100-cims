228a414eac043426cf7482a0ef0d7ef23ef12a7e
Thomas Lebeau <lebeau.thomas@gmail.com>
2023-07-24 11:02:42 +0200

    feat: add email auth provider (#14)

--- src/lib/next-auth.tsx
+++ src/lib/next-auth.tsx
@@ -1,20 +1,42 @@
 import GoogleProvider from 'next-auth/providers/google';
+import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '@/lib/prisma';
 
-export const authOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
-    }),
-  ],
+import prismaClient from '@/lib/prisma';
+import { AuthOptions } from 'next-auth';
+import { Provider } from 'next-auth/providers';
+
+const googleProvider = GoogleProvider({
+  clientId: process.env.GOOGLE_CLIENT_ID as string,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  authorization: {
+    params: {
+      prompt: 'consent',
+      access_type: 'offline',
+      response_type: 'code',
+    },
+  },
+});
+
+const emailProvider = EmailProvider({
+  server: {
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+  },
+  from: process.env.EMAIL_FROM,
+});
+
+const providers: Array<Provider> = [googleProvider];
+
+if (process.env.EMAIL_SERVER_HOST) {
+  providers.push(emailProvider);
+}
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
+  providers: providers,
 };
