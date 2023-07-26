5f6ab81595c695757278d892e3b264d8450c4ee7
Thomas Lebeau <lebeau.thomas@gmail.com>
2023-07-26 09:06:43 +0200

    fix: fix process.env type definition

---
+++ src/types/env.d.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+
+// This file is a module augmentation.
+// Files containing module augmentation must be modules (as opposed to scripts).
+// The difference between modules and scripts is that modules have at least one import/export statement.
+//
+// In order to make TypeScript treat the file as a module, we add one export statement to it.
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_MAPBOX_TOKEN: string;
+
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+
+      EMAIL_SERVER_HOST: string;
+      EMAIL_SERVER_PORT: string;
+      EMAIL_SERVER_USER: string;
+      EMAIL_SERVER_PASSWORD: string;
+    }
+  }
+}
+
+export {};
