518a0a73c6d86a6fe41ca737ce6bf7793a6ce849
Thomas Lebeau <lebeau.thomas@gmail.com>
2023-07-26 10:01:48 +0200

    fix remove unneeded no-ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import getServerSession from '@/lib/get-server-session';
 
-import NoSsr from '@/components/no-ssr';
-
 import SessionProvider from './components/session-provider';
 import Nav from './components/nav';
 import Main from './components/main';
@@ -15,9 +13,7 @@ export default async function Home() {
     <div className="h-screen max-h-screen flex flex-col">
       <SessionProvider session={session}>
         <Nav />
-        <NoSsr>
-          <Main className="grow" />
-        </NoSsr>
+        <Main className="grow" />
       </SessionProvider>
     </div>
   );
