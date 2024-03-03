073b2b290e95ea90a0ad82b5fa15ebeef5647c1c
Thomas Lebeau <lebeau.thomas@gmail.com>
2024-03-03 21:16:52 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! feat: add dd-trace dependency

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,12 +1,9 @@
 export function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    // require("dd-trace").tracer.init({
-    //   service: "100-cims",
-    //   version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
-    //   env: process.env.NEXT_PUBLIC_VERCEL_ENV,
-    //   site: "datadoghq.eu",
-    //   token: process.env.DD_API_KEY,
-    // });
-    require("dd-trace/init");
+    require("dd-trace").init({
+      env: process.env.VERCEL_ENV,
+      version: process.env.VERCEL_GIT_COMMIT_SHA,
+      service: "100-cims",
+    });
   }
 }
