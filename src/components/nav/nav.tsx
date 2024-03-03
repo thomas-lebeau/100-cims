9069e8172845be1810bab4c9a1376d23126b92c3
Thomas Lebeau <lebeau.thomas@gmail.com>
2024-03-03 15:44:18 +0100

    feat: do not instrument in dev environment

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -2,7 +2,6 @@
 
 import { cn } from "@/lib/cn";
 import Link from "next/link";
-import { Button } from "../ui/button";
 import { UserMenu } from "./user-menu";
 
 export default function Nav({ className }: { className?: string }) {
@@ -11,14 +10,6 @@ export default function Nav({ className }: { className?: string }) {
       <Link href="/" className="text-2xl font-bold mr-auto">
         💯🏔️ 100 cims
       </Link>
-      <Button
-        onClick={() => {
-          throw new Error("Whaaaaaaat!!!");
-        }}
-        className="mr-2"
-      >
-        Booom
-      </Button>
       <UserMenu className="flex ml-auto" />
     </nav>
   );
