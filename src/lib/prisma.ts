de9a661b7804cdb2ce0b518af073df29e7f29df2
Thomas Lebeau <lebeau.thomas@gmail.com>
2024-08-04 19:49:56 +0200

    fixup! fixup! fixup! feat: trace getServerSession

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,28 +1,10 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import tracer from "dd-trace";
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined; // eslint-disable-line no-unused-vars
 }
 
 export const prisma = global.prisma || new PrismaClient();
-
-prisma.$use(async (params, next) => {
-  const tags = {
-    "span.kind": "client",
-    "span.type": "sql",
-    "prisma.model": params.model,
-    "prisma.action": params.action,
-  };
-
-  return tracer.trace("prisma.query", { tags }, () => next(params));
-});
-
-prisma.$on("query" as never, (e: Prisma.QueryEvent) => {
-  // add query to prisma span
-  const span = tracer.scope().active();
-  span?.setTag("resource.name", e.query);
-});
 
 if (process.env.NODE_ENV === "development") {
   global.prisma = prisma;
