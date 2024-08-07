3281213c111bc5c92b73ebbf7759b65677a5b5ad
Thomas Lebeau <lebeau.thomas@gmail.com>
2024-08-07 20:08:19 +0200

    fix: filter by comarca (#168)

--- e2e/home.test.ts
+++ e2e/home.test.ts
@@ -8,3 +8,21 @@ test("has user menu", async ({ page }) => {
 
   await expect(page.getByText(USER.email)).toBeVisible();
 });
+
+test("Can filter by comarca", async ({ page }) => {
+  await page.goto("/");
+
+  await page
+    .locator("div")
+    .filter({ hasText: /^AllEssentialsAllAscendedComarca$/ })
+    .getByRole("button")
+    .click();
+
+  await page.getByRole("option", { name: "Alt Penedès" }).click();
+
+  await expect(page.getByRole("complementary")).toContainText("Alt Penedès");
+
+  await page.getByRole("button", { name: "Reset", exact: true }).click();
+
+  await expect(page.getByRole("complementary")).toContainText("Comarca");
+});
