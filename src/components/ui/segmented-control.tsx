9b57c6e1867060ac518f847bd71d2ceda45519d4
Thomas Lebeau <lebeau.thomas@gmail.com>
2025-01-25 18:22:10 +0100

    chore: upgrade to tailwind 4

--- src/components/ui/segmented-control.tsx
+++ src/components/ui/segmented-control.tsx
@@ -31,7 +31,7 @@ const SegmentedControlOption = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-2 py-1 text-xs font-normal ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-2 py-1 text-xs font-normal ring-offset-background transition-all focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-xs",
       className
     )}
     {...props}
