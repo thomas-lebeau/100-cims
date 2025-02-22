54ebaff3f5b719c4109bb2c937572b650bfd0bc0
Thomas Lebeau <lebeau.thomas@gmail.com>
2025-02-22 17:21:02 +0100

    fix: remove unused file

--- src/lib/get-server-session.ts
+++
@@ -1,6 +0,0 @@
-import { authOptions } from "./next-auth";
-import { getServerSession as nextAuthGetServerSession } from "next-auth/next";
-
-export default function getServerSession() {
-  return nextAuthGetServerSession(authOptions);
-}
